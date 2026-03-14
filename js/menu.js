5d6567ad724e935a425eb878e1d25234a49a7cc1
lulusu580544-oss <lulusu580544@gmail.com>
2026-03-14 21:32:51 +0800

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,82 @@
+$(function () {
+
+    $('.text:eq(0)').hover(function () {
+        $('.image img:eq(0)').css('opacity', '1');
+        $('.image img:eq(1)').css('opacity', '0');
+        $('.image img:eq(3)').css('opacity', '0');
+        $('.image img:eq(2)').css('opacity', '0');
+    });
+
+    $('.text:eq(1)').hover(function () {
+        $('.image img:eq(1)').css('opacity', '1');
+        $('.image img:eq(0)').css('opacity', '0');
+        $('.image img:eq(3)').css('opacity', '0');
+        $('.image img:eq(2)').css('opacity', '0');
+    });
+    $('.text:eq(2)').hover(function () {
+        $('.image img:eq(2)').css('opacity', '1');
+        $('.image img:eq(1)').css('opacity', '0');
+        $('.image img:eq(0)').css('opacity', '0');
+        $('.image img:eq(3)').css('opacity', '0');
+    });
+    $('.text:eq(3)').hover(function () {
+        $('.image img:eq(3)').css('opacity', '1');
+        $('.image img:eq(2)').css('opacity', '0');
+        $('.image img:eq(0)').css('opacity', '0');
+        $('.image img:eq(1)').css('opacity', '0');
+    });
+
+
+    //切換漢堡按鈕
+    $('.hamburger').click(function () {
+        //切換 三 & X 加入 & 刪除class
+        $(this).toggleClass('is-active');
+        $('.navgation').toggleClass('show');
+    });
+
+});
+
+
+/* $(window).scroll(function () {
+    //stop為了防止程式多次重複循環
+    if ($(this).scrollTop() > 2000) {
+        $('& a').css("color", "white");
+    }
+    if ($(this).scrollTop() > 4000) {
+        $('& a').css("color", "black");
+    }
+    else if ($(this).scrollTop() < 2000) {
+        $('& a').css("color", "black");
+    }
+})
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 2000) {
+        $('.main-text').css("color", "white");
+    }
+    if ($(this).scrollTop() > 3700) {
+        $('.main-text').css("color", "black");
+    }
+    else if ($(this).scrollTop() < 2000) {
+        $('.main-text').css("color", "black");
+    }
+})
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 2000) {
+        $('.connet img:eq(0)').css('opacity', '0');
+        $('.connet img:eq(1)').css('opacity', '1');
+    }
+    if ($(this).scrollTop() > 4000) {
+        $('.connet img:eq(0)').css('opacity', '1');
+        $('.connet img:eq(1)').css('opacity', '0');
+    }
+    else if ($(this).scrollTop() < 2000) {
+        $('.connet img:eq(0)').css('opacity', '1');
+        $('.connet img:eq(1)').css('opacity', '0');
+    }
+}) */
+
+
+//jquery smoove
+$('.smoove').smoove();
